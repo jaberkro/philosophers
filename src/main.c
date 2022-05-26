04048289cc14cddfe4cc233f0e9d4f0822a65a09
Jorien Aberkrom <jaberkro@student.codam.nl>
2022-05-26 18:41:20 +0200

    parsing added

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/05/23 18:08:37 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/05/25 18:00:30 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/05/26 18:30:49 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,24 @@ int	main(int argc, char **argv)
 {
 	t_data	data;
 
-	if (argc < 5 || argc > 6)
-	{
-		printf("Wrong amount of arguments\n");
+	if (!error_check(argc, argv))
 		return (1);
-	}
-	data = parsing(argc, argv);
-	if (data.error == 1)
+	if (!parsing(argc, argv, &data))
 		return (1);
-	if (gettimeofday(&data.start_time, NULL) < 0)
-	{
-		printf("Gettimeofday failed\n");
-		return (1);
-	}
-	printf("%ld - Starttime of simulation in microseconds\n", \
-	data.start_time.tv_sec * 1000000 + data.start_time.tv_sec);
+	
+	// printf("%d\n", data.philos[0].to_eat);
+	// if (data.error == 1)
+	// 	return (1);
+	// if (gettimeofday(&data.start_time, NULL) < 0)
+	// {
+	// 	printf("Gettimeofday failed\n");
+	// 	return (1);
+	// }
+	// printf("%ld - Starttime of simulation in microseconds\n", \
+	// data.start_time.tv_sec * 1000000 + data.start_time.tv_sec);
 	return (0);
 }
 
 // is 020 valid input?
+// and what about +20?
+// "" can be given as input as well now, better not
