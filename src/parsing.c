b2d2f7bb5fcbfebc6119e3651647ca360c512e18
Jorien Aberkrom <jaberkro@student.codam.nl>
2022-05-26 18:43:01 +0200

    added and initialized philo variable

--- src/parsing.c
+++ src/parsing.c
@@ -6,11 +6,46 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/05/25 13:34:16 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/05/25 17:38:26 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/05/26 18:40:03 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+static int	init_forks(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	data->forks = malloc (data->philosophers * sizeof(int));
+	if (data->forks == NULL)
+		return (print_return("Error: malloc failed\n", 0));
+	while (i < data->philosophers)
+	{
+		data->forks[i] = i + 1;
+		i++;
+	}
+	return (1);
+}
+
+static int	init_philos(t_data *data)
+{
+	unsigned long	i;
+
+	i = 0;
+	data->philos = malloc ((data->philosophers) * sizeof(t_philo));
+	if (data->philos == NULL)
+		return (print_return("Error: Malloc failed", 0));
+	while (i < data->philosophers)
+	{
+		data->philos[i].id = i + 1;
+		data->philos[i].eat_time = 0;
+		data->philos[i].to_eat = data->times_must_eat;
+		data->philos[i].eaten = 0;
+		i++;
+	}
+	return (1);
+}
 
 static unsigned long	atoul(const char *input)
 {
@@ -19,6 +54,8 @@ static unsigned long	atoul(const char *input)
 
 	number = 0;
 	i = 0;
+	if (input[i] == '+')
+		i++;
 	while (input[i] >= '0' && input[i] <= '9')
 	{
 		number = number * 10 + input[i] - '0';
@@ -27,45 +64,21 @@ static unsigned long	atoul(const char *input)
 	return (number);
 }
 
-static int	check_if_digit(char *to_check)
+int	parsing(int argc, char **argv, t_data *data)
 {
-	int	i;
-
-	i = 0;
-	if (!to_check)
-	{
-		printf("invalid input\n");
+	data->philosophers = atoul(argv[1]);
+	data->time_to_die = atoul(argv[2]);
+	data->time_to_eat = atoul(argv[3]);
+	data->time_to_sleep = atoul(argv[4]);
+	data->times_must_eat = -1;
+	if (argc == 6)
+		data->times_must_eat = atoul(argv[5]);
+	data->start_time = get_time();
+	if (!init_philos(data))
 		return (0);
-	}
-	while (to_check[i])
-	{
-		if (to_check[i] < '0' || to_check[i] > '9')
-		{
-			printf("%s: not a whole number\n", to_check);
-			return (0);
-		}
-		i++;
-	}
+	if (!init_forks(data))
+		return (0);
 	return (1);
 }
 
-t_data	parsing(int argc, char **argv)
-{
-	int				i;
-	unsigned long	number;
-	t_data			data;
-
-	i = 1;
-	data.error = 0;
-	while (i < argc)
-	{
-		if (!check_if_digit(argv[i]))
-			data.error = 1;
-		number = atoul(argv[i]);
-		data.inputs[i - 1] = number;
-		i++;
-	}
-	if (argc == 5)
-		data.inputs[4] = 0;
-	return (data);
-}
+	//printf("philos: %lu, die: %lu eat: %lu sleep: %lu times: %lu start: %lu\n", data->philosophers, data->time_to_die, data->time_to_eat, data->time_to_sleep, data->times_must_eat, data->start_time);
