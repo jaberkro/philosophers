fc545543564a9eb121c072c297b066c0a125f080
Jorien Aberkrom <jaberkro@student.codam.nl>
2022-06-04 19:31:08 +0200

    colours added

---
+++ include/colour.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   colour.h                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/06/04 12:56:02 by jaberkro      #+#    #+#                 */
+/*   Updated: 2022/06/04 12:59:47 by jaberkro      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLOUR_H
+# define COLOUR_H
+
+# define RED   "\x1B[31m"
+# define GRN   "\x1B[32m"
+# define YEL   "\x1B[33m"
+# define BLU   "\x1B[34m"
+# define MAG   "\x1B[35m"
+# define CYN   "\x1B[36m"
+# define WHT   "\x1B[37m"
+# define RESET "\x1B[0m"
+
+#endif
